b75a6c3b768e43c3fcc0b62cf80b6a3d7c3e6005
Pavel Lovkii <plovkiy@yandex.ru>
2026-01-22 00:51:13 +0700

    feat: add basic count summarizer

---
+++ recursive-count-summarizer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Pavel Lovkii <plovkiy@yandex.ru>
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link https://github.com/seschustle/example-comments-api-client
+ */
+
+declare(strict_types=1);
+
+function summarizeCounts(string $dirPath): int
+{
+    if (!is_dir($dirPath)) {
+        throw new InvalidArgumentException("$dirPath is not a directory.");
+    }
+
+    $sum = 0;
+    foreach (scandir($dirPath) as $child) {
+        if ($child === '.' || $child === '..') {
+            continue;
+        }
+        
+        if (is_dir($dirPath .'/'. $child)) {
+            $sum += summarizeCounts($dirPath .'/'. $child);
+        }
+        
+        if ($child === 'count') {
+            $sum += file_get_contents($dirPath .'/'. $child);
+        }
+    }
+    
+    return $sum;
+}
+
+echo summarizeCounts(__DIR__ . '/' . $argv[1]);
